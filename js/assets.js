7d61d384afada3a8a2209c74b2ae24b4002c2513
TheMimmzyy <cjc3481@gmail.com>
2021-05-16 23:05:54 -0600

    init repo

---
+++ js/assets.js
@@ -0,0 +1,27 @@
+var questions = [
+    {
+        title: "Which is not a coding language?",
+        choices: ["JavaScript", "CSS", "HTML", "Bootstrap"],
+        answer: "Bootstrap"
+    },
+    {
+        title: "An array is identified as being enclosed by __?",
+        choices: ["quotes", "curly braces", "parentheses", "square brackets"],
+        answer: "square brackets"
+    },
+    {
+        title: "What is the possible amount of variables that can be stored in an array?",
+        choices: ["3", "5", "100", "no limit"],
+        answer: "no limit"
+    },
+    {
+        title: "What language is used for styling web pages?",
+        choices: ["CSS", "C++", "C#", "JavaScript"],
+        answer: "CSS"
+    },
+    {
+        title: "Bootstrap is a ___?",
+        choices: ["Language", "Library", "Framework", "All of the above"],
+        answer: "Framework"
+    }
+];
